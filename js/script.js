df9e17a096615a08686465127ea9423b58121fa6
Mattia Cugusi <m.cugusi287@gmail.com>
2024-11-05 11:40:49 +0400

    js div

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 function mostraDiv(){
     let div = document.getElementById("div1");
-    
+    div.style.display = "block";
+}
+
+function nascondiDiv(){
+    let div = document.getElementById("div1");
+    div.style.display = "none";
 }
